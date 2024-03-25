2929152bd9309399a31ad4351432bdc42584c981
misopog <94299562+misopog@users.noreply.github.com>
2024-03-25 14:31:32 +0100

    did somebody say an update was needed ?

--- js/script.js
+++ js/script.js
@@ -14,3 +14,17 @@ titleChanger = (text, delay) => {
 titleChanger(["misopog", "follow me on twitter", "star my projects"], 2000);
 
 
+document.addEventListener("DOMContentLoaded", function() {
+    const particleContainer = document.getElementById("particle-container");
+    const numParticles = 100; 
+    const delayMultiplier = 0.05; 
+
+    for (let i = 0; i < numParticles; i++) {
+        const particle = document.createElement("div");
+        particle.classList.add("particle");
+        particle.style.left = Math.random() * 100 + 'vw'; 
+        particle.style.bottom = '0'; 
+        particle.style.animationDelay = (i * delayMultiplier) + 's'; 
+        particleContainer.appendChild(particle);
+    }
+});
